70d88f077c28102e66fcac75fb9f4de20c1518fb
Chainarong Janthoom <oofonlove@gmail.com>
2020-09-07 02:20:28 +0700

    Add files via upload

---
+++ assets/basic_head.php
@@ -0,0 +1,21 @@
+<!-- bootstrap & fontawesome -->
+<link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/font-awesome.css" />
+
+    <!-- page specific plugin styles -->
+    <link rel="stylesheet" href="css/jquery-ui.custom.css" />
+    <link rel="stylesheet" href="css/chosen.css" />
+    <link rel="stylesheet" href="css/datepicker.css" />
+    <link rel="stylesheet" href="css/bootstrap-timepicker.css" />
+    <link rel="stylesheet" href="css/daterangepicker.css" />
+    <link rel="stylesheet" href="css/bootstrap-datetimepicker.css" />
+    <link rel="stylesheet" href="css/colorpicker.css" />
+
+    <!-- text fonts -->
+    <link rel="stylesheet" href="css/ace-fonts.css" />
+
+    <!-- ace styles -->
+    <link rel="stylesheet" href="css/ace.css" class="ace-main-stylesheet" id="main-ace-style" />
+
+    <!-- ace settings handler -->
+    <script src="js/ace-extra.js"></script>
